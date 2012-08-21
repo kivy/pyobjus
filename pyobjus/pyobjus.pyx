5eb319796b75ae5870d69366c92318a213212bce
Mathieu Virbel <mat@kivy.org>
2012-08-21 16:42:14 +0200

    wip

--- pyobjus/pyobjus.pyx
+++ pyobjus/pyobjus.pyx
@@ -1,3 +1,7 @@
+__all__ = ('ObjcMethod', )
+
+import re
+
 cdef extern from *:
     ctypedef char* const_char_ptr "const char*"
 
@@ -29,4 +33,26 @@ cdef unsigned int method_list_for_class():
 
 cpdef test():
     print method_list_for_class()
+
+cdef parse_signature(bytes signature):
+    parts = re.split('(\d±)', signature)[:-1]
+    signature_return = parts[0:2]
+    parts = parts[2:]
+    signature_args = zip(parts[0::2], parts[1::2])
+    return signature_return, signature_args
+
+
+class ObjcMethod(object):
+    cdef bytes name
+    cdef bytes signature
+    cdef object signature_return
+    cdef object signature_args
+
+    def __cinit__(self, signature, **kwargs):
+        pass
+
+    def __init__(self, signature, **kwargs):
+        super(ObjcMethod, self).__init__()
+        self.signature = <bytes>signature
+        self.signature_return, self.signature_args = parse_signature(signature)
 
