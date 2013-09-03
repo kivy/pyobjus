3ef7ef9a104e362f988781de528ee966856f28f6
ivpusic <ivpusic@foi.hr>
2013-09-04 01:58:16 +0200

    bridge class updated

--- objc_classes/aux/bridge.h
+++ objc_classes/aux/bridge.h
@@ -1,9 +1,19 @@
 #import <Foundation/Foundation.h>
 #import <CoreMotion/CoreMotion.h>
-#import <UIKit/UIKit.h>
 
 @interface bridge : NSObject
 
 @property (strong, nonatomic) CMMotionManager *motionManager;
+@property (nonatomic) double ac_x;
+@property (nonatomic) double ac_y;
+@property (nonatomic) double ac_z;
+
+@property (nonatomic) double gy_x;
+@property (nonatomic) double gy_y;
+@property (nonatomic) double gy_z;
+
+@property (nonatomic) double mg_x;
+@property (nonatomic) double mg_y;
+@property (nonatomic) double mg_z;
 
 @end
--- objc_classes/aux/bridge.m
+++ objc_classes/aux/bridge.m
@@ -2,25 +2,56 @@
 
 @implementation bridge
 
-static const NSTimeInterval accelerometerMin = 0.01;
+- (id) init {
+    if(self = [super init]) {
+        self.motionManager = [[CMMotionManager alloc] init];
+    }
+    return self;
+}
 
-- (void)startAccel {
-    
-    self.motionManager = [[CMMotionManager alloc] init];
-    self.motionManager.accelerometerUpdateInterval = .2;
-    self.motionManager.gyroUpdateInterval = .2;
+- (void)startAccelerometer {
     
     if ([self.motionManager isAccelerometerAvailable] == YES) {
-        [self.motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue mainQueue] withHandler:^(CMAccelerometerData *accelerometerData, NSError *error) {
-            printf("x: %f\n", accelerometerData.acceleration.x);
-            printf("y: %f\n", accelerometerData.acceleration.y);
-            printf("z: %f\n", accelerometerData.acceleration.z);
+        [self.motionManager startAccelerometerUpdatesToQueue:[[NSOperationQueue alloc] init] withHandler:^(CMAccelerometerData *accelerometerData, NSError *error) {
+            self.ac_x = accelerometerData.acceleration.x;
+            self.ac_y = accelerometerData.acceleration.y;
+            self.ac_z = accelerometerData.acceleration.z;
         }];
     }
 }
 
-- (void) stopAccel {
+- (void)startGyroscope {
+    
+    if ([self.motionManager isGyroAvailable] == YES) {
+        [self.motionManager startGyroUpdatesToQueue:[[NSOperationQueue alloc] init] withHandler:^(CMGyroData *gyroData, NSError *error) {
+            self.gy_x = gyroData.rotationRate.x;
+            self.gy_y = gyroData.rotationRate.y;
+            self.gy_z = gyroData.rotationRate.z;
+        }];
+    }
+}
+
+- (void)startMagnetometer {
+    
+    if (self.motionManager.magnetometerAvailable) {
+        [self.motionManager startMagnetometerUpdatesToQueue:[[NSOperationQueue alloc] init] withHandler:^(CMMagnetometerData *magnetometerData, NSError *error) {
+            self.mg_x = magnetometerData.magneticField.x;
+            self.mg_y = magnetometerData.magneticField.y;
+            self.mg_z = magnetometerData.magneticField.z;
+        }];
+    }
+}
+
+- (void) stopAccelerometer {
     [self.motionManager stopAccelerometerUpdates];
+}
+
+- (void) stopGyroscope {
+    [self.motionManager stopGyroUpdates];
+}
+
+- (void) stopMagnetometer {
+    [self.motionManager stopMagnetometerUpdates];
 }
 
 
