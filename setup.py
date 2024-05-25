eabefdf914a385e3993a23d8b88c992576fe95b2
Mirko Galimberti <me@mirkogalimberti.com>
2024-05-25 11:38:20 +0200

    Remove support for Python 3.7 as reached EOL (#108)

--- setup.py
+++ setup.py
@@ -140,7 +140,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: MacOS',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
